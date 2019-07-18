601282342e153dfb105d6f817581ca1ee74e9746
jarisMA <jarisma@foxmail.com>
2019-07-18 13:29:38 +0800

    密码加密

--- myblog.sql
+++ myblog.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 2019-06-26 12:46:47
--- 服务器版本： 10.1.19-MariaDB
--- PHP Version: 5.5.38
+-- 主机： 127.0.0.1
+-- 生成日期： 2019-07-18 07:27:38
+-- 服务器版本： 10.1.37-MariaDB
+-- PHP 版本： 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `myblog`
+-- 数据库： `myblog`
 --
 
 -- --------------------------------------------------------
@@ -34,6 +36,17 @@ CREATE TABLE `blogs` (
   `author` varchar(20) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- 转存表中的数据 `blogs`
+--
+
+INSERT INTO `blogs` (`id`, `title`, `content`, `createtime`, `author`) VALUES
+(3, '标题A', '内容A', 1561795098493, 'lisi'),
+(4, '标题B', '内容B', 1561799287651, 'zhangsan'),
+(10, 'sdgd', 's', 1561907067619, 'zhangsan'),
+(14, 'dsg', 'sdfgadfga5tur6ty75rty65123', 1561907809386, 'zhangsan'),
+(17, '&lt;script&gt;alert(document.cookie)&lt;/script&gt', '', 1563274524390, 'zhangsan');
+
 -- --------------------------------------------------------
 
 --
@@ -43,22 +56,30 @@ CREATE TABLE `blogs` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `realname` varchar(10) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Indexes for dumped tables
+-- 转存表中的数据 `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `realname`) VALUES
+(1, 'zhangsan', '7ddc404babd8409911491f928a7787bb', '张三'),
+(2, 'lisi', '7ddc404babd8409911491f928a7787bb', '李四');
+
+--
+-- 转储表的索引
 --
 
 --
--- Indexes for table `blogs`
+-- 表的索引 `blogs`
 --
 ALTER TABLE `blogs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- 表的索引 `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
@@ -71,12 +92,15 @@ ALTER TABLE `users`
 -- 使用表AUTO_INCREMENT `blogs`
 --
 ALTER TABLE `blogs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
 --
 -- 使用表AUTO_INCREMENT `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/controller/user.js
+++ src/controller/user.js
@@ -2,10 +2,16 @@ const {
     exec,
     escape
 } = require('../db/mysql')
+const {
+    getPassword
+} = require('../utils/cryp')
 
 const login = (username, password) => {
     //防止登录的sql注入
     username = escape(username)
+
+    //生成加密密码
+    password = getPassword(password)
     password = escape(password)
 
     let sql = `select username, realname from users where username=${username} and password=${password};`
