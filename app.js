493e99e38871744c2b86a924f44d2aab317ee410
jarisMA <jarisma@foxmail.com>
2019-07-21 10:58:24 +0800

    简单添加路由处理

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan'); // 记录日志
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const blogRouter = require('./routes/blog');
+const userRouter = require('./routes/user');
+
 
 var app = express(); // 实例
 
@@ -14,16 +17,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+
+//可在 res.body 中获取post请求的数据
 app.use(express.json()); // 解析post请求（application/json)
 app.use(express.urlencoded({ // 解析post请求（x-www-form-urlencoded）
     extended: false
 }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); // 静态文件路径
 
 // 处理路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/blog', blogRouter);
+app.use('/api/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
