650c7160a898384e7a1fdfccc9c8d0ffdbd8432f
jaris <jarisma@foxmail.com>
2019-07-23 10:40:04 +0800

    日志

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const views = require('koa-views') // 视图
 const json = require('koa-json') // post data 里json处理
 const onerror = require('koa-onerror') // 错误处理
 const bodyparser = require('koa-bodyparser') // post上传的body
-const logger = require('koa-logger') // 日志
+const logger = require('koa-logger') // 在koa下优化后台打印格式
 const session = require('koa-generic-session')
 const RedisStore = require('koa-redis')
+const path = require('path')
+const fs = require('fs')
+const morgan = require('koa-morgan') // 记录日志
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -44,6 +47,21 @@ app.use(async(ctx, next) => {
     const ms = new Date() - start
     console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+
+const ENV = process.env.NODE_ENV
+if (ENV !== 'production') {
+    // 开发环境 / 测试环境
+    app.use(morgan('dev'));
+} else {
+    // 线上环境
+    const logFileName = path.join(__dirname, 'logs', 'access.log')
+    const writeStream = fs.createWriteStream(logFileName, {
+        flags: 'a'
+    })
+    app.use(morgan('combined', {
+        stream: writeStream // 默认为标准输出 process.stdout
+    }));
+}
 
 // session配置
 app.keys = ['joa,sdf.sf_e#25']
